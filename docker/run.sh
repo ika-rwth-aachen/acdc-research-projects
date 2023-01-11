1067eb62c6a6aef2584affab46c169303e6b5d18
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-01-11 18:07:46 +0100

    add dockerfile to create image with gpu support via conda

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,23 @@
-../acdc-notebooks/docker/run.sh
+#!/bin/sh
+
+DIR="$(cd -P "$(dirname "$0")" && pwd)"
+MOUNT_DIR="$(dirname "$DIR")"
+
+# The user inside the docker environment is `jovyan` with uid 1000
+# This setting is predefined in registry.git.rwth-aachen.de/jupyter/profiles/rwth-courses:latest
+# to ensure compatibility with RWTH Jupyter Hub
+# We will give this user id temporarly write permissions to this directory
+setfacl -R -m u:1000:rwx $MOUNT_DIR
+
+docker run \
+--name='ika-acdc-research-projects' \
+--rm \
+--interactive \
+--tty \
+--publish 8888:8888 \
+--volume $MOUNT_DIR:/home/jovyan/acdc \
+--gpus all \
+rwthika/acdc-research-projects:latest
+
+# Remove write permission of user 1000
+setfacl -R -x u:1000 $MOUNT_DIR
